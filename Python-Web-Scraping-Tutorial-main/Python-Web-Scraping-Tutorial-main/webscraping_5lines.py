1579f3f25bcfa3392ec1219308ad3c774d39b019
medac42 <165911246+medac42@users.noreply.github.com>
2024-10-24 09:09:30 +0200

    Delete Python-Web-Scraping-Tutorial-main/Python-Web-Scraping-Tutorial-main directory

--- Python-Web-Scraping-Tutorial-main/Python-Web-Scraping-Tutorial-main/webscraping_5lines.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-response = requests.get("https://en.wikipedia.org/wiki/Web_scraping")
-bs = BeautifulSoup(response.text, "lxml")
-print(bs.find("p").text)
